f5e6cf5be9039f0fd3dc1c7206249bdf4cf1d935
Leah Meromy <lmeromy@gmail.com>
2018-10-15 20:41:57 +0100

    Logic adjusted

--- src/models/prime_checker.js
+++ src/models/prime_checker.js
@@ -13,17 +13,19 @@ PrimeChecker.prototype.bindEvents = function () {
   })
 };
 
-
 PrimeChecker.prototype.numberIsPrime = function (number) {
-  if (number <= 1) {
-    return `No! ${number} is not a prime number.`;
-  }
-  for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
+      if (number <= 1) {
         return `No! ${number} is not a prime number.`;
-    }
-  }
-  return `Yes! ${number} is a prime number.`;
-};
+      }
+      // solution adapted from https://www.w3resource.com/javascript-exercises/javascript-function-exercise-8.php
+      // it seems more efficient since you don't need to check as many numbers for each iteration
+      let root = Math.floor(Math.sqrt(number));
+      for (let i = 2; i <= root; i++) {
+          if (number % i === 0) {
+              return `No! ${number} is not a prime number.`;
+          }
+      }
+      return `Yes! ${number} is a prime number.`;
+  };
 
 module.exports = PrimeChecker;
